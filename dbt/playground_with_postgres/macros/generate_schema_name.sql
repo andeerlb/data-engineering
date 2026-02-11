0bb7d701287192ace833045eab01dc216767874f
Anderson Babinski <andeerlb@gmail.com>
2026-02-11 20:24:39 -0300

    dbt playground with silver,gold,bronze | staging, marts (data marts)

--- dbt/playground_with_postgres/macros/generate_schema_name.sql
+++ dbt/playground_with_postgres/macros/generate_schema_name.sql
@@ -1,5 +1,5 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    
+    {# This macro generates the schema name for a given node. If a custom schema name is provided, it will use that; otherwise, it will default to the target schema defined in the dbt profile. #}
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
     {%- else -%}
---
+++ dbt/playground_with_postgres/models/gold/gold_customers.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='table') }}
+
+with customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+    from {{ ref('silver_orders') }}
+    group by customer_id
+
+)
+
+select
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    co.first_order_date,
+    co.most_recent_order_date,
+    coalesce(co.number_of_orders, 0) as number_of_orders
+
+from {{ ref('silver_customers') }} c
+left join customer_orders co
+    on c.customer_id = co.customer_id
