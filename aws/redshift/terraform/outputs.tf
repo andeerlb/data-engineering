9ea94d7adead567c55b80d62fdf4f3e5817a4e37
Anderson Babinski <andeerlb@gmail.com>
2026-02-11 13:37:09 -0300

    dbt overview

--- aws/redshift/terraform/outputs.tf
+++ aws/redshift/terraform/outputs.tf
@@ -21,3 +21,31 @@ output "aws_region" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.redshift_data_bucket.bucket
 }
+
+output "redshift_copy_commands" {
+  value = <<-EOT
+copy jaffle_shop.customers(id, first_name, last_name)
+from 's3://${aws_s3_bucket.redshift_data_bucket.bucket}/jaffle_shop_customers.csv'
+iam_role '${aws_iam_role.redshift_role.arn}'
+region '${var.aws_region}'
+delimiter ','
+ignoreheader 1
+acceptinvchars;
+
+copy jaffle_shop.orders(id, user_id, order_date, status)
+from 's3://${aws_s3_bucket.redshift_data_bucket.bucket}/jaffle_shop_orders.csv'
+iam_role '${aws_iam_role.redshift_role.arn}'
+region '${var.aws_region}'
+delimiter ','
+ignoreheader 1
+acceptinvchars;
+
+copy stripe.payments(id, orderid, paymentmethod, status, amount, created)
+from 's3://${aws_s3_bucket.redshift_data_bucket.bucket}/stripe_payments.csv'
+iam_role '${aws_iam_role.redshift_role.arn}'
+region '${var.aws_region}'
+delimiter ','
+ignoreheader 1
+acceptinvchars;
+EOT
+}
